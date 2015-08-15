f2ed71616b85ba0184dbb59161b5f9c1ca6e101e
Denis Zdorovtsov <mrtrizer@gmail.com>
2015-08-15 13:58:42 +0300

    Replaced uint32_t to unsigned int.

--- src/imemctrl.h
+++ src/imemctrl.h
@@ -8,7 +8,7 @@ class IMemCtrl
 public:
     virtual ~IMemCtrl(){}
     virtual void setValue(unsigned int n, unsigned int value) = 0;
-    virtual unsigned int getValue(uint32_t n) = 0;
+    virtual unsigned int getValue(unsigned int n) = 0;
 };
 
 #endif // IMEMCTRL_H
